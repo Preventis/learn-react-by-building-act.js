8eb6840de155d1d073bb4ab9dfb0fd3426a8d91e
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-10 10:58:11 +0200

    EDIT/index.html: instructions, EDIT/src: setup playground.js and act.js

--- src/act.js
+++ src/act.js
@@ -1,13 +1,28 @@
 // This will be our file to implement our own React.js — called Act.js
 const root = document.querySelector('#mount');
 
-const greet = () => {
-  const rootFound =
-    root === null
-      ? "<div id='mount' /> is missing"
-      : 'Nice - You setup everything!';
-  console.log(`Hello from Act.js — ${rootFound}`);
+const Act = {
+  createElement(element, content) {
+    console.log('Please implement the creation of elements');
+    // TODO:
+    // Make use of document.createElement to create a DOM node
+    // of the argument type 'element'.
+
+    // TODO:
+    // Add content to the above created node
+
+    // TODO:
+    // Then return the above element
+  }
 };
 
-export default () => greet();
+const ActDOM = {
+  render(element, rootElement) {
+    console.log('Please implement the rendering');
+    // TODO:
+    // Append element as a child to the rootElement
+  }
+};
+
+export { Act, ActDOM };
 
--- src/playground.js
+++ src/playground.js
@@ -1,6 +1,23 @@
 // This will serve as a playground to test and try out our implementation
 // of React.js
-import act from './act.js';
 
-act();
+// We separate Act.js into two modules:
+// 1. Act:    Implements the basic creation of elements
+// 2. ActDOM: Implements DOM manipulation logic
+import { Act, ActDOM } from './act.js';
+
+// We have to first find the root element
+// Act.js uses this to render it's created DOM tree into.
+const root = document.querySelector('#mount');
+
+// Act should provide a function called 'createElement'
+// this function should have two arguments:
+// 1. element - A string that should represent default HTML tags (div, button h1)
+// 2. children - A string that should act as the content of the element
+const firstElement = Act.createElement('div', 'Hello Act.js!');
+
+// ActDOM should provide a function called 'render' which takes an element
+// created by Act (using the above createElement function) and a root DOM node
+// which we queried above ↑
+ActDOM.render(firstElement, root);
 
