e433c65e39e3ba084a35e236d625cfe34f92c0ae
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-10 11:28:12 +0200

    EDTI/src/act.js: solution

--- src/act.js
+++ src/act.js
@@ -3,15 +3,17 @@ const root = document.querySelector('#mount');
 
 const Act = {
   createElement(element, content) {
-    // TODO:
     // Check if element is a functional component (a function)
     // and return the application of the function
-
-    // else return the code below for the ability to create
-    // default DOM node elements further on.
-    const el = document.createElement(element);
-    el.innerHTML = content;
-    return el;
+    if (typeof element === 'function') {
+      return element();
+    } else {
+      // else return the code below for the ability to create
+      // default DOM node elements further on.
+      const el = document.createElement(element);
+      el.innerHTML = content;
+      return el;
+    }
   }
 };
 
