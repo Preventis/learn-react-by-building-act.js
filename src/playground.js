726fe6b77f6c52d18bf5debc717c1e23718d0216
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-10 10:32:41 +0200

    ADD/package.json + index.html + src/ dir

--- src/playground.js
+++ src/playground.js
@@ -1,2 +1,6 @@
-// This will be our playground to test and try out our React implementation
+// This will serve as a playground to test and try out our implementation
+// of React.js
+import act from './act.js';
+
+act();
 
