eec9df7ceddf4061342af8b4d7f3bc3646ccf187
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-11 15:48:18 +0200

    WIP/index.html + server/budo.js: interactive exercise-switching

--- server/budo.js
+++ server/budo.js
@@ -3,26 +3,31 @@ var budo = require('budo');
 var babelify = require('babelify');
 var childProcess = require('child_process');
 
-childProcess.exec('git checkout exercise-00/setup', (err, stdout) => {
-  console.log(stdout, err);
-  childProcess.exec('git branch', (err, stdout) => {
-    console.log(stdout);
+function switchBranch(name) {
+  childProcess.exec(`git checkout ${name}`, (err, stdout) => {
+    console.log(stdout, err);
+    childProcess.exec('git branch', (err, stdout) => {
+      console.log(stdout, err);
+    });
+  });
+}
+
+budo('./src/playground.js', {
+  live: true,
+  stream: process.stdout,
+  port: 8000,
+  browserify: {
+    transform: babelify
+  }
+}).on('connect', function(ev) {
+  const wss = new WebSocket.Server({ port: 1337 });
+  wss.on('connection', ws => {
+    ws.on('message', mess => {
+      const message = JSON.parse(mess);
+      if (message.action === 'next') {
+        switchBranch(message.to);
+      }
+    });
   });
 });
-
-// budo('./src/playground.js', {
-//   live: true,
-//   stream: process.stdout,
-//   port: 8000,
-//   browserify: {
-//     transform: babelify
-//   }
-// }).on('connect', function(ev) {
-//   const wss = new WebSocket.Server({ port: 1337 });
-//   wss.on('connection', ws => {
-//     ws.on('message', mess => {
-//       console.log(mess);
-//     });
-//   });
-// });
 
--- src/playground.js
+++ src/playground.js
@@ -3,6 +3,12 @@
 const ws = new WebSocket('ws://localhost:1337');
 
 ws.onopen = () => {
-  ws.send('commit', { test: 1 });
+  ws.send(JSON.stringify({ action: 'handshake' }));
 };
+
+const button = document.querySelector('#btn-next');
+console.log(button);
+button.addEventListener('click', () => {
+  ws.send(JSON.stringify({ action: 'next', to: 'exercise-00/setup' }));
+});
 
