832034a11dcb1b04cb4a4478ac9337bdb2e27d2b
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-12 15:00:06 +0200

    EDIT/index.html + package.json + server/

--- server/tutorial.js
+++ server/tutorial.js
@@ -1,22 +1,29 @@
 const ws = new WebSocket('ws://localhost:4444');
 
+const tutorialNav = document.querySelector('#tutorial-nav');
+tutorialNav.style.position = 'fixed';
+tutorialNav.style.margin = '40px';
+tutorialNav.style.bottom = 0;
+tutorialNav.style.opacity = 0;
+
 ws.onopen = () => {
   ws.send(JSON.stringify({ action: 'handshake' }));
+  tutorialNav.style.opacity = 1;
 };
 
 const nextButton = document.querySelector('#btn-next');
 if (nextButton) {
   nextButton.addEventListener('click', () => {
-    ws.send(
-      JSON.stringify({ action: 'next', to: 'exercise-08/Rerender-Part1' })
-    );
+    const to = nextButton.innerHTML;
+    ws.send(JSON.stringify({ action: 'next', to: to }));
   });
 }
 
 const solutionButton = document.querySelector('#btn-solution');
 if (solutionButton) {
   solutionButton.addEventListener('click', () => {
-    ws.send(JSON.stringify({ action: 'next', to: 'solution-07/State' }));
+    const to = solutionButton.innerHTML;
+    ws.send(JSON.stringify({ action: 'next', to: to }));
   });
 }
 
