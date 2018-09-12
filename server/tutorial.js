1a74341a43193ca55fa3f69215b896523a0cb482
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-12 11:55:22 +0200

    EDIT/server: integrate budo

---
+++ server/tutorial.js
@@ -0,0 +1,22 @@
+const ws = new WebSocket('ws://localhost:4444');
+
+ws.onopen = () => {
+  ws.send(JSON.stringify({ action: 'handshake' }));
+};
+
+const nextButton = document.querySelector('#btn-next');
+if (nextButton) {
+  nextButton.addEventListener('click', () => {
+    ws.send(JSON.stringify({ action: 'next', to: 'example/TodoApp' }));
+  });
+}
+
+const solutionButton = document.querySelector('#btn-solution');
+if (solutionButton) {
+  solutionButton.addEventListener('click', () => {
+    ws.send(
+      JSON.stringify({ action: 'next', to: 'solution-10/Rerender-children' })
+    );
+  });
+}
+
